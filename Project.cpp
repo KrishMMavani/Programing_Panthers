13c68b86fc5f12d228a1baed35f3f314f2fff128
prathamaAG <117733270+prathamaAG@users.noreply.github.com>
2023-04-27 21:30:57 +0530

    function to search stud

--- Project.cpp
+++ Project.cpp
@@ -26,3 +26,28 @@ void addStudent(Student students[], int& count) {
     // Display a message to indicate that the record was added successfully
     cout << "Student added successfully." << endl;
 }
+// Function to search for a student by name
+void searchStudentByName(Student students[], int count) {
+    string name;
+    // Prompt the user to enter the name of the student to search for
+    cout << "Enter student name: ";
+    cin >> name;
+
+    bool found = false;
+
+    // Search for the student by name in the students array
+    for (int i = 0; i < count; i++) {
+        if (students[i].name == name) {
+            // Display the student's information if they are found
+            cout << "Name: " << students[i].name << endl;
+            cout << "Age: " << students[i].age << endl;
+            cout << "Marks: " << students[i].marks << endl;
+            found = true;
+            break;
+        }
+    }
+    // Display a message if the student is not found
+    if (!found) {
+        cout << "Student not found." << endl;
+    }
+}
