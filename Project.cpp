1bc36f50ab96acb85385596cd430b52a6b05febc
SujalV2011 <131846876+SujalV2011@users.noreply.github.com>
2023-04-27 21:28:45 +0530

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -7,3 +7,22 @@ struct Student {
     int age;
     float marks;
 };
+//Function to add a new student record
+void addStudent(Student students[], int& count) {
+    string name;
+    int age;
+    float marks;
+    // Prompt the user to enter the student's information
+    cout << "Enter student name: ";
+    cin >> name;
+    cout << "Enter student age: ";
+    cin >> age;
+    cout << "Enter student marks: ";
+    cin >> marks;
+    // Create a new student record using the entered information
+    Student student = { name, age, marks };
+    // Add the new student record to the students array
+    students[count++] = student;
+    // Display a message to indicate that the record was added successfully
+    cout << "Student added successfully." << endl;
+}
